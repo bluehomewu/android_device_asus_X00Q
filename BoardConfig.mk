41f8a18f462aa7c3f9cebe8651b87cccde766a3c
emincangencer <emincange@gmail.com>
2020-01-31 23:24:24 +0300

    update vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,7 +237,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-09-05
+VENDOR_SECURITY_PATCH := 2019-12-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
