716f6e91a5b9d3b19a6d76764bd230e90d49bb7a
emincangencer <emincange@gmail.com>
2020-01-31 23:30:30 +0300

    update locations of audio configs

--- device.mk
+++ device.mk
@@ -100,16 +100,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_gain.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_gain.conf \
-		$(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+	$(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
-		$(LOCAL_PATH)/audio/audio_output_policy_24bit.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy_24bit.conf \
-		$(LOCAL_PATH)/audio/mixer_paths_ZE620KL.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL.xml \
+	$(LOCAL_PATH)/audio/audio_output_policy_24bit.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy_24bit.conf \
+	$(LOCAL_PATH)/audio/mixer_paths_ZE620KL.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL.xml \
     $(LOCAL_PATH)/audio/mixer_paths_ZE620KL_EU.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL_EU.xml \
-		$(LOCAL_PATH)/audio/mixer_paths_ZE620KL_EU_24bit.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL_EU_24bit.xml \
-		$(LOCAL_PATH)/audio/mixer_paths_ZE620KL_24bit.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL_24bit.xml \
+	$(LOCAL_PATH)/audio/mixer_paths_ZE620KL_EU_24bit.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL_EU_24bit.xml \
+	$(LOCAL_PATH)/audio/mixer_paths_ZE620KL_24bit.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_ZE620KL_24bit.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-		$(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
+	$(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio/audio_policy_configuration_ZE620KL.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration_ZE620KL.xml \
@@ -130,17 +130,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9335.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9335.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9340.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9340.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes_ZE620KL.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_ZE620KL.xml \
-		$(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-		$(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
-
+	$(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+	$(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/a2dp_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables_ZE620KL.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables_ZE620KL.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/a2dp_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
-    $(TOPDIR)device/asus/X00Q/audio/default_volume_tables_ZE620KL.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables_ZE620KL.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
-
-		#$(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
 
 # BlackPlayer Music Player
 PRODUCT_PACKAGES += \
